79614f456f5c4ef20bc6b4ebb55a9fbc571762f0
Velez <zvelezbara@gmail.com>
2021-10-03 21:12:33 -0400

    Estructura cpp

---
+++ EjercicioExtra.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int sort (int n) {
+    int n2 = n, n1=n, a, mini=9, maxi=0,r=0;
+    while (n1 != 0) {
+        while (n2 != 0) {
+            a = n2 % 10;
+            n2 = n2 / 10;
+            if (a < mini && a > maxi) {
+                mini = a;
+            }
+        }
+        maxi = mini;
+        r = r + mini; r = r *10;
+        mini = 9;
+        n2 = n;
+        n1 = n1 / 10;
+    }
+    r /= 10;
+    return r;
+}
+
+int main() {
+    int a;
+    cin >> a;
+    cout << sort(a) << "\n";
+}
